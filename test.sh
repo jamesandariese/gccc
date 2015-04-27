fda7a38d3f64bf953035947048a6402f9b4d4a3f
James Andariese <james.andariese@locationlabs.com>
2015-04-26 21:01:07 -0700

    added -newgo setting

--- test.sh
+++ test.sh
@@ -2,13 +2,29 @@
 
 cleanup() {
     rm -rf blorp
+    rm -f go
     exit ${1:-1}
 }
 
 trap cleanup HUP INT TERM
 
 cd test
-eval `../gccc windows/amd64 blorp`
+
+if [ x"$1" = x-newgo ];then
+    cat > go <<EOF
+#!/bin/sh
+1>&2 echo "forced by -newgo"
+false
+EOF
+    chmod +x go
+    export PATH="$PWD:$PATH"
+fi
+
+NEWENV="`../gccc windows/amd64 blorp`"
+if [ $? -ne 0 ];then
+    exit 1
+fi
+
 go build
 grep 'This program cannot be run in DOS mode' test.exe || exit 1
 
